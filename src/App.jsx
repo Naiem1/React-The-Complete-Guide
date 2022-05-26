f7eb8d0f06fab5dc81ef5d0433c8c21fed6c4e89
naiem <naiem4288@gamil.com>
2022-05-26 17:35:17 +0600

    cleaner soluation to set condition

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,30 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Harry Potter')}
+            changed={this.nameChangedHandler}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am React App</h1>
@@ -68,27 +92,7 @@ class App extends Component {
           style={style}
           onClick={this.togglePersonHandler}
         >Switch Name</button>
-
-        {
-          this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Harry Potter')}
-              changed={this.nameChangedHandler}
-            >
-              My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div> : null }
+        { persons }
       </div>
     )
   }
